61148eb5aade7f9336d841d34531cbec4184cc36
Aaron Belenky <aaron.b@sealsecurity.io>
2026-07-20 13:30:02 +0300

    CVE-2026-44490+CVE-2026-44494+CVE-2026-44496

--- index.d.cts
+++ index.d.cts
@@ -440,6 +440,8 @@ declare namespace axios {
     http2Options?: Record<string, any> & {
       sessionTimeout?: number;
     };
+    formDataHeaderPolicy?: 'legacy' | 'content-only';
+    redact?: string[];
   }
 
   // Alias
--- index.d.ts
+++ index.d.ts
@@ -513,6 +513,8 @@ export interface AxiosRequestConfig<D = any> {
   http2Options?: Record<string, any> & {
     sessionTimeout?: number;
   };
+  formDataHeaderPolicy?: 'legacy' | 'content-only';
+  redact?: string[];
 }
 
 // Alias
